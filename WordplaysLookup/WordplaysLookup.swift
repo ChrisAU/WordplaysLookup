d0ba703a0f42024bd45525e523f87485dbe5be64
Chris Nevin <cjnevin@gmail.com>
2016-07-05 22:10:15 +0100

    Added redirection to 'see <a...' links to fetch the similar word and return that definition instead.

--- WordplaysLookup/WordplaysLookup.swift
+++ WordplaysLookup/WordplaysLookup.swift
@@ -13,6 +13,10 @@ import Foundation
 public enum WordplaysLookup {
     
     public static func find(word: String, completion: (definition: String?) -> ()) {
+        _find(word: word, completion: completion)
+    }
+    
+    private static func _find(word: String, originalWord: String? = nil, completion: (definition: String?) -> ()) {
         let sanitisedWord = word.lowercased().trimmingCharacters(in: CharacterSet.lowercaseLetters.inverted)
         guard let url = URL(string: "http://www.wordplays.com/definition/\(sanitisedWord)") else {
             completion(definition: nil)
@@ -21,26 +25,42 @@ public enum WordplaysLookup {
         
         func complete(definition: String? = nil) {
             DispatchQueue.main.async {
-                completion(definition: definition)
+                guard let definition = definition else {
+                    completion(definition: nil)
+                    return
+                }
+                if let originalWord = originalWord {
+                    completion(definition: originalWord + " or " + definition)
+                } else {
+                    completion(definition: definition)
+                }
             }
         }
         
         DispatchQueue.global().async {
             guard let
                 response = try? String(contentsOf: url),
-                openTagRange = response.range(of: "<td class=defword>"),
-                closeTagRange = response.substring(from: openTagRange.upperBound).range(of: "</td>") else {
-                complete()
-                return
+                openTagIndex = response.range(of: "<td class=defword>")?.upperBound,
+                closeTagIndex = response.substring(from: openTagIndex).range(of: "</td>")?.lowerBound else {
+                    complete()
+                    return
             }
             
             let definition = response
-                .substring(from: openTagRange.upperBound)
-                .substring(to: closeTagRange.lowerBound)
+                .substring(from: openTagIndex)
+                .substring(to: closeTagIndex)
                 .trimmingCharacters(in: .whitespacesAndNewlines)
             
             guard definition.lengthOfBytes(using: .utf8) > 0 else {
                 complete()
+                return
+            }
+            
+            if let startSeeIndex = definition.range(of: "<a href='/definition/")?.upperBound, endSeeIndex = definition.substring(from: startSeeIndex).range(of: "'>")?.lowerBound {
+                let referenceWord = definition
+                    .substring(from: startSeeIndex)
+                    .substring(to: endSeeIndex)
+                _find(word: referenceWord, originalWord: sanitisedWord, completion: completion)
                 return
             }
             
